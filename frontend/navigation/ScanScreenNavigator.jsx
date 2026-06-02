117b907feb2ffd690afef62c679311a9252225e9
Yaa Asantewaa <antwiyaa17@gmail.com>
2026-06-02 04:53:28 +0000

    Rename CameraView to Camera and update import

--- frontend/navigation/ScanScreenNavigator.jsx
+++ frontend/navigation/ScanScreenNavigator.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Scan from "../screens/Scan";
-import CameraView from "../screens/CameraView";
+import CameraScreen from "../screens/CameraScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +9,7 @@ export default function ScanStackNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ScanHome" component={Scan} />
-      <Stack.Screen name="CameraView" component={CameraView} />
+      <Stack.Screen name="Camera" component={CameraScreen} />
     </Stack.Navigator>
   );
 }
